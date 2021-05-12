57952beb2cc6d28dc120484ba55e6bccf16393bb
srinidhipatil1 <srinidhi.patil.39@gmail.com>
2021-05-12 22:29:57 +0530

    front end main activity

--- app/src/main/java/com/example/workoutlogger/MainActivity.java
+++ app/src/main/java/com/example/workoutlogger/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.workoutlogger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button mondayButton, tuesdayButton, wednesdayButton, thursdayButton, fridayButton, saturdayButton, sundayButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mondayButton=findViewById(R.id.mondayButton);
+        tuesdayButton=findViewById(R.id.tuesdayButton);
+        wednesdayButton=findViewById(R.id.wednesdayButton);
+        thursdayButton=findViewById(R.id.thursdayButton);
+        fridayButton=findViewById(R.id.fridayButton);
+        saturdayButton=findViewById(R.id.saturdayButton);
+        sundayButton=findViewById(R.id.sundayButton);
+
+        mondayButton.setOnClickListener(this);
+        tuesdayButton.setOnClickListener(this);
+        wednesdayButton.setOnClickListener(this);
+        thursdayButton.setOnClickListener(this);
+        fridayButton.setOnClickListener(this);
+        saturdayButton.setOnClickListener(this);
+        sundayButton.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, mondayActivity.class);
+        startActivity(intent);
     }
 }
